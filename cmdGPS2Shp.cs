6a4b565ec475e328a1ef99673947381ea73c73f8
Xintao Liu <xintao.liu@ryerson.ca>
2015-09-28 14:58:06 -0400

    Create cmdGPS2Shp.cs

---
+++ cmdGPS2Shp.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Controls;
+
+namespace ArcSceneKDE
+{
+    /// <summary>
+    /// Command that works in ArcScene or SceneControl
+    /// </summary>
+    [Guid("c9376257-f316-4cab-b943-395b6996cf43")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("ArcSceneKDE.cmdGPS2Shp")]
+    public sealed class cmdGPS2Shp : BaseCommand
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            SxCommands.Register(regKey);
+            ControlsCommands.Register(regKey);
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            SxCommands.Unregister(regKey);
+            ControlsCommands.Unregister(regKey);
+        }
+
+        #endregion
+        #endregion
+
+        private ISceneHookHelper m_sceneHookHelper = null;
+
+        public cmdGPS2Shp()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "KDE3D"; //localizable text
+            base.m_caption = "GPS2Shp";  //localizable text 
+            base.m_message = "Convert raw GPS data to shpfile or geodatabase";  //localizable text
+            base.m_toolTip = "Convert raw GPS data to shpfile or geodatabase";  //localizable text
+            base.m_name = "GPS2Shp_Covertor";   //unique id, non-localizable (e.g. "MyCategory_MyCommand")
+
+            try
+            {
+                //
+                // TODO: change bitmap name if necessary
+                //
+                //string bitmapResourceName = GetType().Name + ".bmp";
+                //base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this command is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            if (hook == null)
+                return;
+
+            try
+            {
+                m_sceneHookHelper = new SceneHookHelperClass();
+                m_sceneHookHelper.Hook = hook;
+                if (m_sceneHookHelper.ActiveViewer == null)
+                {
+                    m_sceneHookHelper = null;
+                }
+            }
+            catch
+            {
+                m_sceneHookHelper = null;
+            }
+
+            if (m_sceneHookHelper == null)
+                base.m_enabled = false;
+            else
+                base.m_enabled = true;
+
+            // TODO:  Add other initialization code
+
+        }
+
+        /// <summary>
+        /// Occurs when this command is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            // TODO: Add cmdGPS2Shp.OnClick implementation
+            FormGPS2Shp pfrm = new FormGPS2Shp();
+            pfrm.ShowDialog();
+        }
+
+        #endregion
+    }
+}
